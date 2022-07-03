d194536a70a6a7e40710120f248fa1905d9f8950
leeyeun <dpdms0109@naver.com>
2022-07-04 00:00:02 +0900

    #15 - Feat:사용자 프로필 생성

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,19 @@ import PostModal from "./components/modal/PostModal";
 import ProductAlert from "./components/modal/ProductAlert";
 import ProductModal from "./components/modal/ProductModal";
 import ProfileModal from "./components/modal/ProfileModal";
+import MyProfile from "./components/profile/MyProfile";
 import "./reset.css";
 function App() {
     return (
         <div className="App">
             <BrowserRouter>
-                <Route exact path="/" component={ProfileModal} />
+                <Route exact path="/profilemodal" component={ProfileModal} />
                 <Route exact path="/logout" component={Logout} />
                 <Route exact path="/postmodal" component={PostModal} />
                 <Route exact path="/postdelete" component={PostAlert} />
                 <Route exact path="/productmodal" component={ProductModal} />
                 <Route exact path="/productdelete" component={ProductAlert} />
+                <Route exact path="/myprofile" component={MyProfile} />
             </BrowserRouter>
         </div>
     );
---
+++ src/components/profile/MyProfile.jsx
@@ -0,0 +1,37 @@
+import TopBasicNav from "../TopBasicNav";
+import basicProfile from "../../assets/basic-profile-img.svg";
+import messageImg from "../../assets/icon/icon-message-circle2.svg";
+import shareIcon from "../../assets/icon/icon-share.svg";
+
+import "./MyProfile.css";
+export default function MyProfile() {
+    return (
+        <div className="profileWrap">
+            <TopBasicNav />
+            <div className="temp-wrap">
+                <a href="/">
+                    <p className="followCount followers">2950</p>
+                    <p className="followName followers">followers</p>
+                </a>
+                <img className="userImage" src={basicProfile} alt="" />
+                <a href="/">
+                    <p className="followCount following">128</p>
+                    <p className="followName following">followings</p>
+                </a>
+            </div>
+
+            <p className="userName">애월읍 위니브 감귤공장</p>
+            <p className="userID">@ weniv_Mandarin</p>
+            <p className="userIntro">
+                애월읍 감귤 전국 배송, 귤따기 체험, 감귤 농장
+            </p>
+            <button className="profileCircleBtn chat">
+                <img src={messageImg} alt="" className="messageImg" />
+            </button>
+            <button className="profileFollowBtn">팔로우</button>
+            <button className="profileCircleBtn share">
+                <img src={shareIcon} alt="" className="shareIcon" />
+            </button>
+        </div>
+    );
+}
