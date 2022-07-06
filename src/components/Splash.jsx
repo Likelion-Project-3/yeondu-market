0c9377403615a6467023936f78cbff20a561f8df
hyuni97 <mezio959@gmail.com>
2022-07-07 01:57:00 +0900

    #1 - Feat:splash 화면 '

--- src/components/Splash.jsx
+++ src/components/Splash.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import './splash.css';
-import '../reset.css'
+import '../reset.css';
 
 function Splash() {
     return (
-        <div>
-            <h1>화면 로딩중</h1> 
+        <>
+            <h1 className="headTitle">화면 로딩중</h1> 
             <section>
                 <p className="colorPea">완두콩</p>
-                <h2>연두 마켓</h2>
+                <h2 className="mainMarket">연두 마켓</h2>
             </section>
-        </div>
+        </>
     );
 }
 
