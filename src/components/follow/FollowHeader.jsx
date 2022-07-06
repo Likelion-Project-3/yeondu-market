df0640dfc8bb71cd8a0a61260b5445c8f6117e3e
leeyeun <dpdms0109@naver.com>
2022-07-06 15:36:09 +0900

    #34 - Feat:팔로워 페이지 개발

---
+++ src/components/follow/FollowHeader.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function FollowHeader() {
+    return (
+        <header className="topMainNav">
+            <button className="prevBtn"></button>
+            <p>Followers</p>
+        </header>
+    );
+}
+
+export default FollowHeader;
