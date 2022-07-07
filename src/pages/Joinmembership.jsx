5e80266f1abf77c8527b53ad9a032ed8f697db0b
hyuni97 <mezio959@gmail.com>
2022-07-07 18:16:32 +0900

    #50-Rename:머지된 파일명 변경으로 인하여 내용 변경

--- src/pages/Joinmembership.jsx
+++ src/pages/Joinmembership.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import Join from "./Join";
+import idPwFrom from "./idPwFrom";
 import LoginButton from "./LoginButton";
 
 function Joinmembership(props){
     return(
     <div className="mainLogin">
         <h1 className="singupTitle">이메일로 회원가입</h1>
-        <Join/>
+        <idPwFrom/>
         <LoginButton value="회원가입"/>
     </div>
     );
