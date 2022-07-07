5e9db466e543b73c261ee728fdabe4fc4e96c207
hyuni97 <mezio959@gmail.com>
2022-07-07 18:13:01 +0900

    #49-Rename:머지된 파일명 변경으로 인하여 내용 변경

--- src/pages/LoginEmail.jsx
+++ src/pages/LoginEmail.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import  './LoginEmail.css';
-import Join from "./Join";
+import idPwFrom from "./idPwFrom";
 import LoginButton from "./LoginButton";
 
 function LoginEmail() {
     return(
         <div className="mainLogin">
         <h1 className="singupTitle">로그인</h1>
-        <Join/>
+        <idPwFrom/>
         <LoginButton value="로그인"/>
         <div className="nextPage">
             <a href="/joinmembership" className="nextPage">이메일로 회원가입</a>
