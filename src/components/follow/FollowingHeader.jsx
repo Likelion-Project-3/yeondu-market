9cc151bd09652a476e4afaea304cc3ad3772867f
leeyeun <dpdms0109@naver.com>
2022-07-08 10:34:21 +0900

    #59-Design : follow classname변경

--- src/components/follow/FollowingHeader.jsx
+++ src/components/follow/FollowingHeader.jsx
@@ -4,7 +4,7 @@ import { useHistory } from "react-router-dom";
 function FollowingHeader() {
     let history = useHistory();
     return (
-        <header className="topMainNav">
+        <header className="topMainNavFollow">
             <button
                 className="prevBtn"
                 onClick={() => {
