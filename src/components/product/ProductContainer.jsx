113e502247f447c8d9ff3510aa1fe923c1bdc069
leeyeun <dpdms0109@naver.com>
2022-07-09 22:50:00 +0900

    #69 - Feat:프로필에 판매중인 상품 연결

--- src/components/product/ProductContainer.jsx
+++ src/components/product/ProductContainer.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import ProductCard from "./ProductCard";
-
+import "./ProductContainer.css";
 function ProductContainer() {
     return (
-        <div>
-            <ProductCard />
+        <div className="productContainer">
+            <h1 className="productOnSale">판매 중인 상품</h1>
+            <section className="productSection">
+                <ProductCard />
+            </section>
         </div>
     );
 }
