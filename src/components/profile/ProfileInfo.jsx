cc7252b6a92d99a96c6977512568c2600741c2ff
leeyeun <dpdms0109@naver.com>
2022-07-14 17:41:18 +0900

    #84 - Feat:a태그 ->link태그로 변경

--- src/components/profile/ProfileInfo.jsx
+++ src/components/profile/ProfileInfo.jsx
@@ -1,25 +1,29 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import BasicProfileImg from "../common/BasicProfileImg";
 import ProfileBtn from "./ProfileBtn";
 import "./ProfileInfo.css";
 
 function ProfileInfo({ profileInfo }) {
+    const username = localStorage.getItem("username");
+    const followLink = `/${username}/follower`;
+    const folloingLink = `/${username}/folloing`;
     return (
         <div className="infoWrap">
             <div className="tempWrap">
-                <a href="/followerlist">
+                <Link to={followLink}>
                     <p className="followCount followers">
                         {profileInfo.profile.followerCount}
                     </p>
                     <p className="followName followers">followers</p>
-                </a>
+                </Link>
                 <BasicProfileImg size="lg" />
-                <a href="/">
+                <Link to={folloingLink}>
                     <p className="followCount following">
                         {profileInfo.profile.followingCount}
                     </p>
                     <p className="followName following">followings</p>
-                </a>
+                </Link>
             </div>
             <div className="">
                 <p className="userName">{profileInfo.profile.username}</p>
