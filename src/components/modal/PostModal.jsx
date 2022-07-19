25522821177336f4f96244fc20542bceecbca21d
Nayoung Gu <skdud000322@naver.com>
2022-07-19 10:53:21 +0900

    #99 - Feat: 게시글 삭제 기능

--- src/components/modal/PostModal.jsx
+++ src/components/modal/PostModal.jsx
@@ -1,11 +1,35 @@
+import { BASE_URL } from "../constants/baseUrl";
+import axios from "axios";
 import "./profileModal.css";
 
-function PostModal() {
+function PostModal(props) {
+    const deletePost = async () => {
+        // const url = BASE_URL + "/post/62d60b3a82fdcc712f4d4713";
+        const post_id = props.data.post.id;
+        const url = BASE_URL + `/post/${post_id}`;
+        const token = localStorage.getItem("token");
+
+        try {
+            const res = await axios(url, {
+                method: "DELETE",
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                    "Content-type": "application/json",
+                },
+            });
+            alert("삭제되었습니다.");
+            console.log(res);
+        } catch (err) {
+            // 404페이지로 이동
+            console.log(err);
+        }
+    };
+
     return (
         <div className="ModalBtnWrap">
-            <a href="/postdelete" className="ModalBtn">
+            <button className="ModalBtn" onClick={deletePost}>
                 삭제
-            </a>
+            </button>
             <a href="/postmodify" className="ModalBtn">
                 수정
             </a>
