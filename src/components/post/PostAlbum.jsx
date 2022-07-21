658c4804b70d868da39febc0cd3b5768cdab5c10
leeyeun <dpdms0109@naver.com>
2022-07-21 14:22:51 +0900

    #126 - Feat:앨범형 게시글

---
+++ src/components/post/PostAlbum.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import PostAlbumItem from "./PostAlbumItem";
+import "./PostAlbum.css";
+import { useEffect } from "react";
+function PostAlbum({ postList }) {
+    console.log("!", postList);
+    const [imageList, setImageList] = useState([]);
+    const post = postList.post;
+    //image가 없는 post는 전달 x
+
+    useEffect(() => {
+        const newList = post.filter((post) => post.image !== "");
+        setImageList(newList);
+        console.log("newList", newList);
+    }, []);
+    console.log("imageList", imageList);
+
+    return (
+        <div className="albumWrap">
+            {imageList && imageList.length > 0
+                ? imageList.map((imageList, id) => {
+                      return (
+                          <div key={id} className="postAlbum">
+                              <PostAlbumItem imageList={imageList} />
+                          </div>
+                      );
+                  })
+                : "null"}
+        </div>
+    );
+}
+
+export default PostAlbum;
