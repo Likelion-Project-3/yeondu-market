288df44daa16fa58490913e5e65e39b150a7fb6a
sujin <soojin9877@naver.com>
2022-07-25 14:23:51 +0900

    #152 - Fix: /home으로 이동

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -12,40 +12,37 @@ import Home from "./Home";
 
 function Login() {
     const url = BASE_URL + '/user/login';
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+    const initialInput = { email: "", password: "" };
+    const [input, setInput] = useState(initialInput);
     const [cautionMsg, setCautionMsg] = useState('');
-    // const [errMsg, setErrMsg] = useState('');
     const [success, setSuccess] = useState(false);
 
     const userRef = useRef();
     useEffect(() => {
         userRef.current.focus();
     }, [])
-
+    
     const [isActive, setIsActive] = useState(false);
     const isPassedLogin = () => {
-        return email.match(/^[a-zA-Z0-9+-\_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/) &&  password.length > 6
+        return input.email.match
+        (/^[a-zA-Z0-9+-\_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/) &&  input.password.length > 5
             ? setIsActive(true)
             : setIsActive(false);
     };
 
-    const emailFuc = (e) => {
-        setEmail(e.target.value);
-    };
-
-    const passwordFuc = (e) => {
-        setPassword(e.target.value);
+    const handleChange = (e) => {
+        const { value, name } = e.target;
+        setInput({ ...input, [name]: value });
     };
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        console.log(email, password);
+        console.log(input.email, input.password);
         try {
             const response = await axios.post(url, {
                 "user": {
-                    "email": email,
-                    "password": password
+                    "email": input.email,
+                    "password": input.password
                 },
             });
             
@@ -55,37 +52,32 @@ function Login() {
                 console.log("dngkgkgdngkg하하하하");
                 setIsActive(false);
                 setCautionMsg('* 이메일 또는 비밀번호가 일치하지 않습니다.');
-                setEmail('');      
-                setPassword('');
+                // setInput({ 
+                //     email: "",
+                //     password: ""
+                // });   
             } else if (response.data.user.intro) {
                 localStorage.setItem('token', response.data.user.token);
                 const token = localStorage.getItem('token');
-                console.log(token);
                 localStorage.setItem('accountname', response.data.user.accountname);
                 const accountname = localStorage.getItem('accountname');
-                console.log(accountname);
                 localStorage.setItem('username', response.data.user.accountname);
                 const username = localStorage.getItem('username');
                 setSuccess(true);
-            } else {
-                
-            };
-            
-            } catch (err) {
-            // if (!err?.response) {
-            //     setErrMsg('No Server response');
-            // } else if (err.response?.status === 400) {
-            //     setErrMsg('Missing Email or Password');
-            // } else {
-            //     setErrMsg('errorrororor');
-            // }
+            }
+        } catch (err) {
+            console.error(err);
         }
-    }
+    };
+
+    const goHome = () => {
+        window.location.href = "/home";
+    };
 
     return (
         <>
             {success ? (
-                <Home />
+                goHome()
             ) : (
                 <div className="mainLogin">
                     <h1 className="singupTitle">로그인</h1>
@@ -95,14 +87,14 @@ function Login() {
                                 이메일
                             </label>
                             <input
-                                onChange={emailFuc}
+                                onChange={handleChange}
                                 onKeyUp={isPassedLogin}
                                 ref={userRef}
                                 type="email"
                                 name="email"
                                 id="loginEmail"
                                 placeholder="이메일 주소를 입력해주세요"
-                                value={email}
+                                value={input.email}
                                 required
                             />
                         </div>
@@ -111,13 +103,13 @@ function Login() {
                                 비밀번호
                             </label>
                             <input
-                                onChange={passwordFuc}
+                                onChange={handleChange}
                                 onKeyUp={isPassedLogin}
                                 type="password"
-                                name="passwordEmail"
+                                name="password"
                                 id="passwordEmail"
                                 placeholder="비밀번호를 입력해주세요"
-                                value={password}
+                                value={input.password}
                             />
                             <strong className={setIsActive ? 'cautionText none' : 'cautionText'}>{cautionMsg}</strong>
                         </div>
@@ -136,5 +128,5 @@ function Login() {
             )}
         </>
     );
-    }
+}
 export default Login;
