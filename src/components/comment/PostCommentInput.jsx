8932990224c9567ad5f64d98acce71e4d7cab83f
Nayoung Gu <skdud000322@naver.com>
2022-07-27 16:36:43 +0900

    #194 - Feat: 게시글 댓글 작성 기능

--- src/components/comment/PostCommentInput.jsx
+++ src/components/comment/PostCommentInput.jsx
@@ -4,15 +4,48 @@ import BasicProfileImg from "../common/BasicProfileImg";
 import axios from "axios";
 import "./PostCommentInput.css";
 
-function PostCommentInput() {
+function PostCommentInput(props) {
     const [text, setText] = useState("");
     const [profileImg, setProfileImg] = useState("");
+    const [isActive, setIsActive] = useState(false);
     const token = localStorage.getItem("token");
     const sendBtn = document.querySelector(".uploadBtn");
+
     const onChange = (e) => {
         setText(e.target.value);
         if (text.length !== 0) {
             sendBtn.classList.remove("disabled");
+        }
+    };
+
+    const onKeyUp = () => {
+        setIsActive(text.length > 0 ? true : false);
+    };
+
+    const onSubmit = async (e) => {
+        e.preventDefault();
+
+        const url = BASE_URL + `/post/${props.postId}/comments`;
+        const comment = {
+            comment: {
+                content: text,
+            },
+        };
+
+        try {
+            const res = await axios(url, {
+                method: "POST",
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                    "Content-type": "application/json",
+                },
+                data: comment,
+            });
+            console.log("res: ", res.data.comment);
+            setText("");
+            props.handleSubmit(); // 댓글 작성 후 댓글 리스트 새로고침
+        } catch (err) {
+            console.log(err);
         }
     };
 
@@ -38,7 +71,7 @@ function PostCommentInput() {
     }, []);
 
     return (
-        <form className="PostCommentForm">
+        <form className="PostCommentForm" onSubmit={onSubmit}>
             <h2 className="ir">댓글 입력</h2>
             <BasicProfileImg size="xs" src={profileImg} />
             <label htmlFor="commentInput" className="ir">
@@ -49,10 +82,15 @@ function PostCommentInput() {
                 placeholder="댓글 입력하기..."
                 className="textInput"
                 onChange={onChange}
+                onKeyUp={onKeyUp}
                 value={text}
                 id="commentInput"
             />
-            <input type="submit" value="게시" className="uploadBtn disabled" />
+            <input
+                type="submit"
+                value="게시"
+                className={`uploadBtn ${!isActive ? "disabled" : ""}`}
+            />
         </form>
     );
 }
--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -4,15 +4,35 @@ import PostComment from "../components/comment/PostComment";
 import PostCommentInput from "../components/comment/PostCommentInput";
 import "../pages/style/Post.css";
 import { BASE_URL } from "../components/constants/baseUrl";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
 
 function Post() {
     const token = localStorage.getItem("token");
     const [postData, setPostData] = useState("");
+    const [comments, setComments] = useState([]);
     const { postId } = useParams();
 
+    // 댓글 업데이트 반영하기
+    const getComments = useCallback(async () => {
+        const url = BASE_URL + `/post/${postId}/comments`;
+
+        try {
+            const res = await axios(url, {
+                method: "GET",
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                    "Content-type": "application/json",
+                },
+            });
+            setComments(res.data.comments.reverse());
+        } catch (err) {
+            console.log(err);
+        }
+    }, [comments]);
+
+    // 기존 게시글 정보 가져오기
     useEffect(() => {
         const getPostPage = async () => {
             const url = BASE_URL + `/post/${postId}`;
@@ -32,8 +52,8 @@ function Post() {
         };
 
         getPostPage();
+        getComments();
     }, []);
-    console.log(postData);
 
     return (
         <>
@@ -53,10 +73,13 @@ function Post() {
             </div>
             <h2 className="ir">댓글 목록</h2>
             <div className="commentWrap">
-                {/* {postData.comment ? <PostComment /> : null} 추후 댓글 불러올 예정*/}
-                <PostComment />
+                {comments.length > 0
+                    ? comments.map((comment, index) => {
+                          return <PostComment comment={comment} key={index} />;
+                      })
+                    : null}
             </div>
-            <PostCommentInput />
+            <PostCommentInput postId={postId} handleSubmit={getComments} />
         </>
     );
 }
