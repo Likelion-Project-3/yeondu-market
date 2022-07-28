c0ce1b6c77ccbefd2a9433fefe660d64e3302e10
sujin <soojin9877@naver.com>
2022-07-28 10:53:11 +0900

    #191 - Feat: 검색 기능

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,23 +1,30 @@
 import TapMenu from "../components/common/TapMenu";
 import "../pages/style/Home.css";
+import { useState } from "react";
+import Search from "./Search";
 
 function Home() {
     const token = localStorage.getItem("token");
+    const [search, setSearch] = useState(false);
 
+    const goSearch = () => {
+        setSearch(true);
+    };
+ 
     return (
         <>
-            {token === null ? (
-                (window.location = "/")
+            {search ? (
+                <Search />
             ) : (
                 <>
                     <header className="topMainNav">
                         <h1>연두마켓 피드</h1>
-                        <button />
+                        <button type="button" onClick={goSearch} />
                     </header>
                     <main className="homeContentNonFeed">
                         <div></div>
                         <p>유저를 검색해 팔로우 해보세요!</p>
-                        <button>검색하기</button>
+                        <button type="button" className="searchBtn" onClick={goSearch}>검색하기</button>
                     </main>
                     <TapMenu />
                 </>
