93e88ebf086795c5c9a2d3e427138e5d0e6f5428
leeyeun <dpdms0109@naver.com>
2022-07-28 17:24:09 +0900

    #226 - 로그아웃 기능 구현

--- src/components/common/TopBasicNav.jsx
+++ src/components/common/TopBasicNav.jsx
@@ -1,22 +1,29 @@
 import ProfileModal from "../modal/ProfileModal";
 import "./TopBasicNav.css";
 import TopMenuComponent from "./TopMenuComponent";
-import React,{useState} from "react";
+import React, { useState } from "react";
 
 function TopBasicNav() {
-    const[isOpenModal, setIsOpenModal] = useState(false);
-    const ModalOpen = () =>{
+    const [isOpenModal, setIsOpenModal] = useState(false);
+    const ModalOpen = () => {
         setIsOpenModal(!isOpenModal);
-    }
-    const handleOpenModal = () => {
-        setIsOpenModal(true);
-    }
-    const handleCloseModal =() =>{
-        setIsOpenModal(false);
-    }
-    return (<>
-        <TopMenuComponent topclassName="topBasicNav" inputtype="notext" rightclassName="moreBtn" type="button" handlerRightBtn={ModalOpen}/>
-        {isOpenModal && (<ProfileModal onCilck={ModalOpen}/>)}
+    };
+
+    return (
+        <>
+            <TopMenuComponent
+                topclassName="topBasicNav"
+                inputtype="notext"
+                rightclassName="moreBtn"
+                type="button"
+                handlerRightBtn={ModalOpen}
+            />
+            {isOpenModal && (
+                <ProfileModal
+                    setIsOpenModal={setIsOpenModal}
+                    isOpenModal={isOpenModal}
+                />
+            )}
         </>
     );
 }
