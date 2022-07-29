2090b467a762552144c70877ac8789957df0f9fd
Nayoung-Gu <skdud000322@naver.com>
2022-07-29 14:04:03 +0900

    #240 - Design: 채팅관련 텍스트 수정

--- src/pages/ChatRoom.jsx
+++ src/pages/ChatRoom.jsx
@@ -7,13 +7,14 @@ function ChatRoom() {
             <TopChatNav />
             <main className="chatRoomMain">
                 <div className="chatWrapper">
+                    <p className="chat left">안녕하세요! 점메추님</p>
                     <p className="chat left">
-                        옷을 인생을 그러므로 없으면 것은 이상의 것은 우리의 것을
-                        위하여, 뿐이다. 이상의 청춘의 뼈 따뜻한 그들의 그와
-                        약동하다 대고, 못할 넣는 풍부하게 뛰노는 인생의 힘이다.
+                        방울 토마토 사용한 레시피 하나만 추천해주세여
                     </p>
-                    <p className="chat left">안녕하세요. 완두콩 사고 싶어요!</p>
-                    <p className="chat right">네 말씀하세요!</p>
+                    <p className="chat right">그럼요!</p>
+                    <p className="chat right">
+                        새우나 닭가슴살 올린 샐러드 파스타 추천이요..🤤!
+                    </p>
                     <p className="chat right img"></p>
                 </div>
             </main>
