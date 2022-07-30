285f35e64e4e6ad89f0d17d7906b10e2d2a1a894
sujin <soojin9877@naver.com>
2022-07-31 03:51:44 +0900

    #276 - Feat: 검색 클릭시 검색 페이지로 이동

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,19 +2,16 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import { BASE_URL } from "../components/constants/baseUrl";
 import PostItem from "../components/post/PostItem";
-import Search from "./Search";
 import TapMenu from "../components/common/TapMenu";
 import "../pages/style/Home.css";
 
 function Home() {
     const token = localStorage.getItem("token");
-    // const [search, setSearch] = useState(false);
-
-    // const goSearch = () => {
-    //     setSearch(true);
-    // };
-
     const [post, setPost] = useState([]);
+
+    const goSearch = () => {
+        window.location.href = "/search";
+    };
 
     useEffect(() => {
         const getUserFeed = async () => {
@@ -37,7 +34,7 @@ function Home() {
         <>
             <header className="topMainNav">
                 <h1>연두마켓 피드</h1>
-                <button type="button" />
+                <button type="button" onClick={goSearch} />
             </header>
             {post.length !== 0 ? (
                 post.map((post, id) => {
@@ -51,7 +48,11 @@ function Home() {
                 <main className="homeContentNonFeed">
                     <div />
                     <p>유저를 검색해 팔로우 해보세요!</p>
-                    <button type="button" className="searchBtn">
+                    <button
+                        type="button"
+                        className="searchBtn"
+                        onClick={goSearch}
+                    >
                         검색하기
                     </button>
                 </main>
