cf73fa90f91207abcf43f3672e7aa876efbb5bf0
sujin <soojin9877@naver.com>
2022-07-31 02:38:02 +0900

    #276- Feat: 피드 띄움

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,40 +1,68 @@
+import { useEffect } from "react";
+import axios from "axios";
+import { BASE_URL } from "../components/constants/baseUrl";
 import TapMenu from "../components/common/TapMenu";
 import "../pages/style/Home.css";
 import { useState } from "react";
 import Search from "./Search";
+// import PostList from "../components/post/PostList";
+import PostItem from "../components/post/PostItem";
 
 function Home() {
     const token = localStorage.getItem("token");
-    const [search, setSearch] = useState(false);
+    // const [search, setSearch] = useState(false);
 
-    const goSearch = () => {
-        setSearch(true);
-    };
+    // const goSearch = () => {
+    //     setSearch(true);
+    // };
+
+    const [post, setPost] = useState([]);
+
+    useEffect(() => {
+        const getUserFeed = async () => {
+            try {
+                const response = await axios.get(BASE_URL + "/post/feed", {
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                    },
+                });
+                console.log(response);
+                setPost(response.data.posts);
+            } catch (err) {
+                console.error(err);
+            }
+        };
+        getUserFeed();
+    }, []);
 
     return (
         <>
-            {search ? (
+            {/* {search ? (
                 <Search />
-            ) : (
-                <>
-                    <header className="topMainNav">
-                        <h1>연두마켓 피드</h1>
-                        <button type="button" onClick={goSearch} />
-                    </header>
-                    <main className="homeContentNonFeed">
-                        <div />
-                        <p>유저를 검색해 팔로우 해보세요!</p>
-                        <button
-                            type="button"
-                            className="searchBtn"
-                            onClick={goSearch}
-                        >
-                            검색하기
-                        </button>
-                    </main>
-                    <TapMenu />
-                </>
-            )}
+            ) : ( */}
+            <>
+                <header className="topMainNav">
+                    <h1>연두마켓 피드</h1>
+                    <button type="button" />
+                </header>
+                {/* <main className="homeContentNonFeed">
+                    <div />
+                    <p>유저를 검색해 팔로우 해보세요!</p>
+                    <button type="button" className="searchBtn">
+                        검색하기
+                    </button>
+                </main> */}
+                <TapMenu />
+
+                {post.map((post, id) => {
+                    return (
+                        <div key={id}>
+                            <PostItem post={post} />
+                        </div>
+                    );
+                })}
+            </>
+            {/* )} */}
         </>
     );
 }
