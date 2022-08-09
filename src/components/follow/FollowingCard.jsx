74cdf36d57b26f40bf55150c9bc5d1530c88b3a3
leeyeun <dpdms0109@naver.com>
2022-08-09 22:30:22 +0900

    #229 - Feat:팔로잉 리스트에 언팔로우api 연결

--- src/components/follow/FollowingCard.jsx
+++ src/components/follow/FollowingCard.jsx
@@ -1,12 +1,61 @@
+import axios from "axios";
 import React from "react";
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import { BASE_URL } from "../constants/baseUrl";
 
 function FollowingCard({ followingList }) {
-    const [isfollow, setIsFollow] = useState(false);
+    const token = localStorage.getItem("token");
 
     const handleFollowBtn = () => {
-        setIsFollow(!isfollow);
+        handleSubmitUnFollow();
+        // console.log(followingList.isfollow);
+        // if (followingList.isfollow === true) {
+        //     handleSubmitUnFollow();
+        //     console.log(followingList.isfollow);
+        // } else {
+        //     handleSubmitFollow();
+        // }
+    };
+
+    //언팔로우
+    const handleSubmitUnFollow = async () => {
+        try {
+            const res = await axios(
+                BASE_URL + `/profile/${followingList.accountname}/unfollow`,
+                {
+                    method: "DELETE",
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                        "Content-type": "application/json",
+                    },
+                }
+            );
+            console.log("언팔로우", res);
+            console.log(res.data.profile.isfollow);
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+    //팔로우
+    const handleSubmitFollow = async () => {
+        try {
+            const res = await axios(
+                BASE_URL + `/profile/${followingList.accountname}/follow`,
+                {
+                    method: "POST",
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                        "Content-type": "application/json",
+                    },
+                }
+            );
+            console.log("팔로우", res);
+            console.log(res.data.profile.isfollow);
+        } catch (err) {
+            console.log(err);
+        }
     };
 
     const userUrl = `/profile/${followingList.accountname}`;
@@ -26,7 +75,6 @@ function FollowingCard({ followingList }) {
                     <p className="followerIntro">{followingList.intro}</p>
                 </div>
             </Link>
-            {/* 팔로우를 하고 있을 땐 취소, 하지 않을땐 팔로우,, */}
             {followingList.isfollow === true ? (
                 <button className="cancelBtn" onClick={handleFollowBtn}>
                     취소
