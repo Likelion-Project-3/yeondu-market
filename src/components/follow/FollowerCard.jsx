6df99195e763587bfbe401142343e1082865243a
leeyeun <dpdms0109@naver.com>
2022-08-09 22:23:32 +0900

    #229 - Feat:팔로우 리스트 버튼에 api연결

--- src/components/follow/FollowerCard.jsx
+++ src/components/follow/FollowerCard.jsx
@@ -1,12 +1,59 @@
+import axios from "axios";
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation, useParams } from "react-router-dom";
+import { BASE_URL } from "../constants/baseUrl";
 
 function FollowerCard({ followerList }) {
-    const [isfollow, setIsFollow] = useState(false);
+    const token = localStorage.getItem("token");
 
     const handleFollowBtn = () => {
-        setIsFollow(!isfollow);
+        if (followerList.isfollow === true) {
+            handleSubmitUnFollow();
+            console.log(followerList.isfollow);
+        } else {
+            handleSubmitFollow();
+        }
     };
+
+    const handleSubmitFollow = async () => {
+        try {
+            const res = await axios(
+                BASE_URL + `/profile/${followerList.accountname}/follow`,
+                {
+                    method: "POST",
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                        "Content-type": "application/json",
+                    },
+                }
+            );
+            console.log("팔로우", res);
+            console.log(res.data.profile.isfollow);
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+    //언팔로우
+    const handleSubmitUnFollow = async () => {
+        try {
+            const res = await axios(
+                BASE_URL + `/profile/${followerList.accountname}/unfollow`,
+                {
+                    method: "DELETE",
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                        "Content-type": "application/json",
+                    },
+                }
+            );
+            console.log("언팔로우", res);
+            console.log(res.data.profile.isfollow);
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
     const userUrl = `/profile/${followerList.accountname}`;
     return (
         <li className="followItem">
