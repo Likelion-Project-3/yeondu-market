ddea4ce952b5d5dea39ea241374484ee55c630e1
Nayoung Gu <skdud000322@naver.com>
2022-08-10 16:51:56 +0900

    #289 - Feat: 로딩 컴포넌트

---
+++ src/components/comment/ContentLoading.jsx
@@ -0,0 +1,13 @@
+import "./ContentLoading.css";
+
+function ContentLoading() {
+    return (
+        <div className="contentLoading">
+            <div className="loadingCircle"></div>
+            <div className="loadingCircle"></div>
+            <div className="loadingCircle"></div>
+        </div>
+    );
+}
+
+export default ContentLoading;
