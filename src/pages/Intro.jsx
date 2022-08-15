be4b2b2f9b46b2d95bae5254b4f1f8b36ddbf9ce
sujin <soojin9877@naver.com>
2022-08-15 11:26:15 +0900

    #313 - Design: 완두콩 이미지 가운데 정렬

--- src/pages/Intro.jsx
+++ src/pages/Intro.jsx
@@ -7,7 +7,9 @@ function Intro() {
         <div className="loginMain">
             <h1 className="headTitle">로그인</h1>
             <section className="loginSection">
-                <div className="pea">완두콩</div>
+                <div className="peaWrap">
+                    <div className="pea">완두콩</div>
+                </div>
                 {/* <a href="https://www.flaticon.com/kr/free-icons/" title=" 아이콘"> 아이콘  제작자: Freepik - Flaticon</a> */}
                 <article className="centerWebLogin">
                     <button className="kakao" type="button">
