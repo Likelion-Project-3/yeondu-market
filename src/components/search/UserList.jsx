44751064899ed3b82deb0d7680e1e47ca6fbd9d1
sujin <soojin9877@naver.com>
2022-08-20 17:02:52 +0900

    #326 - Refactor: BasicProfileImg 컴포넌트 적용

--- src/components/search/UserList.jsx
+++ src/components/search/UserList.jsx
@@ -1,24 +1,15 @@
 import { Link } from "react-router-dom";
-import defaultProfile from "../../assets/basic-profile-pea.png";
+import BasicProfileImg from "../common/BasicProfileImg";
 import "./UserList.css";
 
 function UserList(props) {
     const searchUser = props;
     const userId = searchUser.accountname.slice(2);
 
-    const onErrorImg = (e) => {
-        e.target.src = defaultProfile;
-    };
-
     return (
-        <Link to={`/myprofile/${userId}`}>
+        <Link to={`/profile/${userId}`}>
             <li className="userList" key={props.key}>
-                <img
-                    src={props.image}
-                    alt={props.alt}
-                    onError={onErrorImg}
-                    className="userProfileImg"
-                />
+                <BasicProfileImg size="md" src={props.image} />
                 <div className="userInfo">
                     <p className="username">{props.username}</p>
                     <p className="accountname">{props.accountname}</p>
